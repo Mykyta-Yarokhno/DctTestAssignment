18870c839d8b0a382ebc4304e31160add016ea3d
Mykyta <mykytayarokhno@gmail.com>
2024-11-06 17:10:51 +0200

    Add project files.

---
+++ DctTestAssignment/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DctTestAssignment
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
